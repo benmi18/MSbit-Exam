2b02ea62a2732154b2e7e92f0f8b78cfcbe9b365
BenM <benm@codeoasis.com>
2020-01-11 21:28:46 +0200

    vanilla tilt

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, ViewChild, ElementRef } from "@angular/core";
 import { ProductModal } from "src/app/modals/product.modal";
+import VanillaTilt from "vanilla-tilt";
 
 @Component({
   selector: "app-product-card",
   templateUrl: "./product-card.component.html",
   styleUrls: ["./product-card.component.scss"]
 })
-export class ProductCardComponent {
+export class ProductCardComponent implements OnInit {
   @Input() product: ProductModal;
+
+  ngOnInit() {
+    VanillaTilt.init(document.querySelectorAll(".card-container"), {
+      max: 20,
+      speed: 700
+    });
+  }
 }
